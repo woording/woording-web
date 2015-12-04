0d7eb624f294d3747ed9a26840490929faa1a793
cor <cor@pruijs.nl>
2015-12-04 19:07:42 +0100

    Remove gulpfile.js and install-dependencies.sh

--- install-dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sudo apt-get install python3
-sudo apt-get install python3-pip
-sudo pip3 install virtualenv
-sudo pip3 install Flask
-sudo apt-get install nodejs
-sudo npm install -g gulp
-npm install
-
