8f9d313c9a3dafe7c17e625d08d163c201c54848
cor <cor@pruijs.nl>
2015-12-04 19:04:57 +0100

    Remove static/

--- static/js/app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module("myApp", ['ngDialog', 'ngCookies']);
-
-app.config(function($interpolateProvider){
-    $interpolateProvider.startSymbol('[[').endSymbol(']]');
-});
-
-app.config(function($cookiesProvider) {
-	$cookiesProvider.defaults.path = '/';
-});
