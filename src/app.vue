4e5bd67bc8ed548c0cfabac58d5b5cf287270d60
cor <cor@pruijs.nl>
2015-12-05 01:27:19 +0100

    Add basic data retrieval

--- src/app.vue
+++ src/app.vue
@@ -1,8 +1,27 @@
+<style>
+	
+	body {
+	   font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif; 
+	   font-weight: 300;
+
+	}
+</style>
 
 <template>
 
 <div id="app"> 
 	<site-header></site-header>
+	<p>username: {{user.username}}</p>
+	<p>email: {{user.email}}</p>
+	<p>lists:</p>
+	<ul>
+		<li v-for="list in user.lists">
+			{{list.listname}}			
+		</li>
+		
+	</ul>
+	<p>JSON result for user:</p>
+	<pre>{{ user | json }}</pre>
 </div>
 
 </template>
@@ -13,8 +32,39 @@
 import SiteHeader from './components/ui/header.vue'
 
 export default {
+	ready: function() {
+		this.getUser()
+
+	},
+
 	components: {
 		SiteHeader
+	},
+
+	data: function() {
+		return {
+			user : {
+
+			}
+		}
+	},
+
+	methods : {
+		getUser : function() {
+			var data =  {
+				'username' : 'cor',
+				'password' : 'Hunter2'
+			}
+
+			this.$http.post('http://test.woording.com/authenticate', data, function(data, status, request) {
+
+				this.$http.post('http://test.woording.com/cor', data, function(data, status, request) {
+					this.user = data
+					console.log(this.user)
+				})
+			})
+
+		}
 	}
 }
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 var Vue = require('vue');
 var App = require('./app.vue');
+var VueRouter = require('vue-router');
+var VueResource = require('vue-resource');
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
 
 new Vue({
-  el: 'body',
-  components: {
-    app: App
-  }
+	el: 'body',
+
+
+	ready: function() {
+
+
+	},
+
+	components: {
+		app: App
+	}
+
 });
