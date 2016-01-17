8e51594cd3fe0ae0e4e7d8ea50dfa1eade2c540c
cor <cor@pruijs.nl>
2016-01-17 16:03:39 +0100

    Improve pre-training options

--- src/components/main-window/list-trainer.vue
+++ src/components/main-window/list-trainer.vue
@@ -1,10 +1,34 @@
 <style lang="sass">
 #list-trainer {
-	h1 {
-		font-size: 2rem;
-		color: #FF9800;
-		font-weight: normal;
+	color: #212121;
+
+	#list-info {
+		padding: .5rem .5rem 0 .5rem;
+
+		h1 {
+			font-size: 2rem;
+			color: #FF9800;
+			font-weight: normal;
+		}
 	}
+
+	#options {
+		padding: .5rem .5rem 0 .5rem;
+
+		h3 {
+			padding: 0;
+			margin-top: .5rem;
+		}
+	}
+
+	pre {
+		font-family: Courier;
+		padding: 0;
+		margin: 0;
+		white-space: pre-wrap;
+
+	}
+
 
 	flex: 1;
 	@media(min-width: 768px) {
@@ -15,28 +39,45 @@
 
 <template>
 <div id="list-trainer">
-	<div id="options">
-		<pre>
-			{{ list | json }}
-		
-		</pre>
+
+
+	<div id="list-info">
 		<h1>
-			{{ list.listname }} 
+			{{ list.listname }}
 		</h1>
-		<br>
-		<input type="radio" id="1" v-model="selected" value="{{ list.language_1_tag }}">
+	</div>
+
+
+	<div id="options">
+
+		<h3>LANGUAGE</h3>
+		<input name="language" type="radio" id="1" v-model="options.selectedLanguage" value="{{ list.language_1_tag }}" />
 		<label for="1">{{ list.language_1_tag }} ➡ {{ list.language_2_tag }} </label>
 		<br>
-		<input type="radio" id="2" v-model="selected" value="{{ list.language_2_tag }}">
-		<label for="1">{{ list.language_2_tag }} ➡ {{ list.language_1_tag }} </label>
+
+		<input name="language" type="radio" id="2" v-model="options.selectedLanguage" value="{{ list.language_2_tag }}" checked />
+		<label for="2">{{ list.language_2_tag }} ➡ {{ list.language_1_tag }} </label>
 		<br>
-		<input type="radio" id="both" v-model="selected" value="both">	
-		<label for="both">Both</label>
+
+		<input name="language" type="radio" id="both" v-model="options.selectedLanguage" value="both" />	
+		<label for="both">both</label>
+
+		<h3>MODE</h3>
+		<input name="mode" type="radio" id="mode-1" v-model="options.selectedMode" value="quiz"  checked/>
+		<label for="mode-1">quiz</label>
+
+		<h3>OPTIONS</h3>
+		<input type="checkbox" id="caseSensitive" v-model="options.caseSensitive" />
+		<label for="caseSensitive">case sensitive</label>
 		<br>
-		<input type="checkbox" id="case" v-model="case">
-		<label for="case">Case sensitive?</label>
+
+		<input type="checkbox" id="ignoreTremas" v-model="options.ignoreTremas" />
+		<label for="ignoreTremas">ignore tremas</label>
+		<br>
 
 		<button v-on:click="startTraining">Start</button>
+
+		<pre>{{ options | json }}</pre>
 	</div>
 </div>
 </template>
@@ -49,8 +90,12 @@ export default {
 		return {
 			list: {},
 
-			selected: '',
-			case: false
+			options: {
+				selectedLanguage: '',
+				selectedMode: '',
+				caseSensitive: false,
+				ignoreTremas: false
+			}
 		}
 	},
 
