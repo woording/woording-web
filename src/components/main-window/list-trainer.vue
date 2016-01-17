270789281e0b6dc368fb78cd52ab476af269fe48
cor <cor@pruijs.nl>
2016-01-17 15:13:04 +0100

    Fix overflow and flexbox issues, add some styles

--- src/components/main-window/list-trainer.vue
+++ src/components/main-window/list-trainer.vue
@@ -1,31 +1,54 @@
 <style lang="sass">
+#list-trainer {
+	h1 {
+		font-size: 2rem;
+		color: #FF9800;
+		font-weight: normal;
+	}
 
+	flex: 1;
+	@media(min-width: 768px) {
+		overflow: auto;
+	}
+}
 </style>
 
 <template>
+<div id="list-trainer">
 	<div id="options">
-		Questioned language: 
+		<pre>
+			{{ list | json }}
+		
+		</pre>
+		<h1>
+			{{ list.listname }} 
+		</h1>
 		<br>
 		<input type="radio" id="1" v-model="selected" value="{{ list.language_1_tag }}">
-		<label for="1">{{ list.language_1_tag }}</label>
+		<label for="1">{{ list.language_1_tag }} ➡ {{ list.language_2_tag }} </label>
 		<br>
 		<input type="radio" id="2" v-model="selected" value="{{ list.language_2_tag }}">
-		<label for="2">{{ list.language_2_tag }}</label>
+		<label for="1">{{ list.language_2_tag }} ➡ {{ list.language_1_tag }} </label>
 		<br>
 		<input type="radio" id="both" v-model="selected" value="both">	
 		<label for="both">Both</label>
-
 		<br>
 		<input type="checkbox" id="case" v-model="case">
 		<label for="case">Case sensitive?</label>
+
+		<button v-on:click="startTraining">Start</button>
 	</div>
+</div>
 </template>
 
 <script>
 export default {
+
 	data: function() {
+
 		return {
 			list: {},
+
 			selected: '',
 			case: false
 		}
@@ -40,7 +63,12 @@ export default {
 	methods : {
 		startPractice: function(list) {
 			this.list = list
+		},
+
+		startTraining: function() {
+			alert('go go go')
 		}
+
 	}
 }
 </script>
