e768696c55dbdb89ac59ee5519f0afe6bb2cdc57
cor <cor@pruijs.nl>
2016-01-18 15:00:26 +0100

    Who doesn't like options

--- src/components/main-window/list-trainer.vue
+++ src/components/main-window/list-trainer.vue
@@ -62,20 +62,24 @@
 		<h3>OPTIONS</h3>
 		
 		<div v-show="options.selectedMode == 'quiz'">
-			<input name="language" type="radio" id="1" v-model="options.quiz.selectedLanguage" value="0" checked/>
-			<label for="1">{{ list.language_1_tag }} ➡ {{ list.language_2_tag }} </label>
+			<input name="quiz-language" type="radio" id="quiz-language-1" v-model="options.quiz.selectedLanguage" value="0" checked/>
+			<label for="quiz-language-1">{{ list.language_1_tag }} ➡ {{ list.language_2_tag }} </label>
 			<br>
 
-			<input name="language" type="radio" id="2" v-model="options.quiz.selectedLanguage" value="1" />
-			<label for="2">{{ list.language_2_tag }} ➡ {{ list.language_1_tag }} </label>
+			<input name="quiz-language" type="radio" id="quiz-language-2" v-model="options.quiz.selectedLanguage" value="1" />
+			<label for="quiz-language-2">{{ list.language_2_tag }} ➡ {{ list.language_1_tag }} </label>
 			<br>
 
-			<input name="language" type="radio" id="both" v-model="options.quiz.selectedLanguage" value="2" />
-			<label for="both">both</label>
+			<input name="quiz-language" type="radio" id="quiz-language-both" v-model="options.quiz.selectedLanguage" value="2" />
+			<label for="quiz-language-both">both</label>
 			<br>
 
 			<input type="checkbox" id="caseSensitive" v-model="options.quiz.caseSensitive" />
 			<label for="caseSensitive">case sensitive</label>
+			<br>
+
+			<input type="checkbox" id="randomizeOrder" v-model="options.quiz.randomizeOrder" />
+			<label for="randomizeOrder">randomize order</label>
 			<br>
 
 			<input type="checkbox" id="ignorePunctuation" v-model="options.quiz.ignorePunctuation" />
@@ -86,8 +90,23 @@
 			<label for="ignoreTremas">ignore tremas</label>
 			<br>
 
-			<input type="checkbox" id="randomizeOrder" v-model="options.quiz.randomizeOrder" />
-			<label for="randomizeOrder">randomize order</label>
+		</div>
+
+		<div v-show="options.selectedMode == 'multipleChoice'">
+			<input name="language" type="radio" id="multipleChoice-language-1" v-model="options.multipleChoice.selectedLanguage" value="0" checked/>
+			<label for="multipleChoice-language-1">{{ list.language_1_tag }} ➡ {{ list.language_2_tag }} </label>
+			<br>
+
+			<input name="language" type="radio" id="multipleChoice-language-2" v-model="options.multipleChoice.selectedLanguage" value="1" />
+			<label for="multipleChoice-language-2">{{ list.language_2_tag }} ➡ {{ list.language_1_tag }} </label>
+			<br>
+
+			<input name="language" type="radio" id="multipleChoice-language-both" v-model="options.multipleChoice.selectedLanguage" value="2" />
+			<label for="multipleChoice-language-both">both</label>
+			<br>
+
+			<input type="checkbox" id="multipleChoice-randomizeOrder" v-model="options.multipleChoice.randomizeOrder" />
+			<label for="multipleChoice-randomizeOrder">randomize order</label>
 			<br>
 		</div>
 
@@ -121,13 +140,14 @@ export default {
 				quiz: {
 					selectedLanguage: 0, // 0 = language_1 first, 1 = language_2 first, 2 = both
 					caseSensitive: false,
+					randomizeOrder: false,
 					ignorePunctuation: false,
-					ignoreTremas: false,
-					randomizeOrder: false
+					ignoreTremas: false
 				},
 
 				multipleChoice: {
-
+					selectedLanguage: 0, // 0 = language_1 first, 1 = language_2 first, 2 = both
+					randomizeOrder: false
 				}
 			}
 		}
@@ -139,16 +159,34 @@ export default {
 			var listname = this.$parent.$route.params.listname
 			var gamemode = this.options.selectedMode
 
-			var selectedLanguage = this.options.quiz.selectedLanguage + ""
-			var caseSensitive = this.options.quiz.caseSensitive ? "1" : "0"
-			var ignorePunctuation = this.options.quiz.ignorePunctuation ? "1" : "0"
-			var ignoreTremas = this.options.quiz.ignoreTremas ? "1" : "0"
+			var result = '/' + username + '/' + listname + '/practice/' + gamemode 
 
-			var modifiers = selectedLanguage + caseSensitive + ignorePunctuation + ignoreTremas
+			var modifiers = ""
 
-			let result = '/' + username + '/' + listname + '/practice/' + gamemode + '/' + modifiers 
+			switch (this.options.selectedMode) {
 
-			return result
+				case 'quiz': 
+					// this should probbably be replaced using .map() 
+					var selectedLanguage = this.options.quiz.selectedLanguage + ""
+					var caseSensitive = this.options.quiz.caseSensitive ? "1" : "0"
+					var randomizeOrder = this.options.quiz.randomizeOrder ? "1" : "0"
+					var ignorePunctuation = this.options.quiz.ignorePunctuation ? "1" : "0"
+					var ignoreTremas = this.options.quiz.ignoreTremas ? "1" : "0"
+
+					modifiers = selectedLanguage + caseSensitive + randomizeOrder + ignorePunctuation + ignoreTremas
+					break
+
+				case 'multipleChoice':
+					// this should probbably be replaced using .map() 
+					var selectedLanguage = this.options.multipleChoice.selectedLanguage + ""
+					var randomizeOrder = this.options.multipleChoice.randomizeOrder ? "1" : "0"
+
+					modifiers = selectedLanguage + randomizeOrder
+					break
+			}
+
+
+			return '/' + username + '/' + listname + '/practice/' + gamemode + '/' + modifiers
 		}
 
 	},
