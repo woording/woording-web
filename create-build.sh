14a902b498a2cc7d9147ae22914194633e79bf1b
cor <cor@pruijs.nl>
2016-01-26 10:24:08 +0100

    Add create-build.sh

---
+++ create-build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Delete the old build
+rm -rf build.zip
+rm -rf build/
+
+# Create a new build
+npm run build
+
+mkdir build
+mkdir build/dist/
+
+# Copy files 
+cp index.html build/index.html
+cp dist/build.js build/dist/build.js
+cp -R img build/img 
+cp -R css build/css
+cp favicons/* build/
+
+zip -r build.zip build
