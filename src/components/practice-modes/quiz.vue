5d47d4d1ad6272dd8a8a6e2c8787f2741852ee65
cor <cor@pruijs.nl>
2016-01-26 17:40:34 +0100

    Add basic modifier decoding

--- src/components/practice-modes/quiz.vue
+++ src/components/practice-modes/quiz.vue
@@ -14,6 +14,7 @@
 <div id="trainer-quiz">
 	<h1>top kek</h1>
 	<h2>{{ list.listname }}</h2>
+	<pre>{{ modifiers | json }}</pre>
 	<pre>{{ list | json  }}</pre>
 </div>
 </template>
@@ -26,12 +27,14 @@ export default {
 
 	data () {
 		return {
-			list: {}
+			list: {},
+			modifiers: {}
 		}
 	},
 
 	ready () {
 		this.fetchList()
+		this.decodeModifiers()
 	},
 
 	methods : {
@@ -47,6 +50,16 @@ export default {
 			store.fetchList(username, listname).then((list) => {
 				updateList(list)
 			})
+		},
+
+		decodeModifiers() {
+			// Create an array from the modifiers in the URL
+			let modifiers = this.$parent.$route.params.modifiers.split("")
+			let converteModifier = x => x > 1 ? x : x == 1 
+			let convertedModifiers = modifiers.map(converteModifier)
+
+			this.modifiers = { nonConvertedModifiers: modifiers, convertedModifiers: convertedModifiers }
+
 		}
 	}
 
