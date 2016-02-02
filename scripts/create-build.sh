59e3af3de6a1f10d1911fb349d08504b5d5ffb0d
cor <cor@pruijs.nl>
2016-02-02 22:37:12 +0100

    Improve scripts

--- scripts/create-build.sh
+++ scripts/create-build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Go to the project root
+cd ..
+
 # Delete the old build
 rm -rf build.zip
 rm -rf build/
@@ -7,15 +10,15 @@ rm -rf build/
 # Create a new build
 npm run build
 
+# Create new build dir
 mkdir build
 mkdir build/dist/
 
-# Copy files 
+# Copy files to new build dir 
 cp index.html build/index.html
 cp dist/build.js build/dist/build.js
+
 cp -R img build/img 
 cp -R css build/css
-cp favicons/* build/
-cp run-server.sh build/run-server.sh
 
-ls -laG
+cp favicons/* build/
---
+++ scripts/travis-deploy-woording-web.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd /var/www
+
+# remove the previous version
+rm -rf woording-web-old
+
+# keep the current version in case something goes wrong
+mv woording-web woording-web-old
+
+# extract the new package
+tar -xzf package.tgz
+
+# remove the .tgz file as it is now no longer needed
+rm package.tgz
+
+# the package _should_ contian a folder called build, rename it to woording-web
+mv build woording-web
+
