b092cb64a46aaa56912b04ec48f016fe157cf1d0
LeonGr <leongrasmeijer@gmail.com>
2016-06-02 18:26:57 +0200

    yay

--- src/main.js
+++ src/main.js
@@ -80,6 +80,8 @@ router.afterEach(function () {
 
 router.beforeEach(function(transition){
     if (!globals.getCookie('logvalue') && !store.username){
+        console.log('no log in bro')
+        alert('you are not logged in, you will be redirected to the login page')
         router.go({path : "/"})
     }
     transition.next()
